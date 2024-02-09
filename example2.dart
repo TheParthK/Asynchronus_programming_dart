b90b518b42d99ceee84d534b8fe85a32b5b062e0
Parth Kalia <95091403+TheParthK@users.noreply.github.com>
2024-02-09 11:03:19 +0530

    Feb 9

--- example2.dart
+++ example2.dart
@@ -1,1 +1,23 @@
-// error handlinng
+// error handling
+
+import 'dart:async';
+
+void main(List<String> args) async {
+  print(await getName(firstName: '', lastName: 'Doe'));
+}
+
+
+Future<String> getName({
+  required String firstName,
+  required String lastName,
+}){
+  if(firstName.isEmpty || lastName.isEmpty){
+    throw FirstOrLastNameMissingException();
+  } else {
+    return Future.value('$firstName $lastName');
+  }
+}
+
+class FirstOrLastNameMissingException implements Exception{
+  const FirstOrLastNameMissingException();
+}
